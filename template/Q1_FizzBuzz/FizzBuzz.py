6ad49cf8e3bf640d81b4d7e44b217be1ef21bf21
長澤春希 <nagasawa_h@nagasawaMacBook-Air.local>
2022-05-24 00:29:27 +0900

    delete unused files and solve Q1 and Q2

--- template/Q1_FizzBuzz/FizzBuzz.py
+++ template/Q1_FizzBuzz/FizzBuzz.py
@@ -2,8 +2,14 @@ for num in range(1, 101):
     string = ''
 
     # ここから記述
-
-
+    if num % 5 == 0 and num % 3 == 0:   # 15の倍数の場合
+        string = 'FizzBuzz'
+    elif num % 5 == 0:   # 5の倍数かつ3の倍数でない場合
+        string = 'Buzz'
+    elif num % 3 == 0:   # 3の倍数かつ5の倍数でない場合
+        string = 'Fizz'
+    else:
+        string = str(num)
     # ここまで記述
 
     print(string)
--- template/Q2_Search/Search.py
+++ template/Q2_Search/Search.py
@@ -11,7 +11,25 @@ def main():
 def serch_index(sorted_array, target_number):
 
     # ここから記述
+    """
 
+    :param sorted_array: list of int sorted in accending order
+    :param target_number: target number that you want to find
+    :return: the idx of target_number if target_number exists in sorted_array else -1
+    """
+    candidate_array = sorted_array   # 探索対象のarray
+    while len(candidate_array) > 0:   # 探索対象とするarrayに要素が存在する限り繰り返す
+        center_index = len(candidate_array)//2
+        center_value = candidate_array[center_index]   # 比較対象とする要素
+
+        if center_value == target_number:   # center_valueと合致した場合は探索終了
+            return center_value
+
+        elif center_value > target_number:
+            candidate_array = candidate_array[:center_index]   # center_valueより小さい値を探索対象とする
+
+        else:
+            candidate_array = candidate_array[center_index+1:]   # center_valueより大きい値を探索対象とする
 
     # ここまで記述
 
